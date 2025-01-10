afc1b5115db7c335b5e2345320fbdb354a421037
saarcos <sebasarcosc@gmail.com>
2025-01-09 22:04:53 -0500

    Fix para incluir el campo de rol al obtener usuarios

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,11 +2,14 @@ import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import Usuario from '../models/userModel.js';
 import 'dotenv/config';
+import Rol from '../models/roleModel.js';
 
 export const login = async (req, res) => {
   const { email, password } = req.body;
   try {
-    const usuario = await Usuario.findOne({ where: { email } });
+    const usuario = await Usuario.findOne({ where: { email },
+      include: [{ model: Rol, as: 'rol', attributes: ['nombrerol'] }],
+    });
     if (!usuario) {
       return res.status(404).json({ error: 'Usuario no encontrado' });
     }
@@ -15,7 +18,7 @@ export const login = async (req, res) => {
       return res.status(401).json({ error: 'Contraseña incorrecta' });
     }
     const token = jwt.sign(
-      { id: usuario.id_usuario, id_rol: usuario.id_rol, email: usuario.email, nombre: usuario.nombre },
+      { id: usuario.id_usuario, id_rol: usuario.id_rol, rol: usuario.rol.nombrerol, email: usuario.email, nombre: usuario.nombre, apellido: usuario.apellido },
       process.env.JWT_SECRET,
       { expiresIn: '1h' }
     );
@@ -29,8 +32,10 @@ export const login = async (req, res) => {
       user: {
         id: usuario.id_usuario,
         nombre: usuario.nombre, 
+        apellido: usuario.apellido,
         email: usuario.email,
-        id_rol: usuario.id_rol
+        id_rol: usuario.id_rol,
+        rol: usuario.rol.nombrerol, // Agregar rol a la respuesta
       }
     });
   } catch (error) {
